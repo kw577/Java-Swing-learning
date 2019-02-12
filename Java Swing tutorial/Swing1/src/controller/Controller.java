cb819ae335c1b7640babd61d9edb8586139266c5
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2019-02-12 15:29:45 +0100

    MVC, Controller, połącznie backend-frontend, przeniesienie plikow dotyczacych interfejsu do pakietu gui

---
+++ Java Swing tutorial/Swing1/src/controller/Controller.java
@@ -0,0 +1,77 @@
+package controller;
+
+import gui.FormEvent;
+import model.AgeCategory;
+import model.Database;
+import model.EmploymentCategory;
+import model.Gender;
+import model.Person;
+
+public class Controller {
+	
+	Database db = new Database();
+	
+	public void addPerson(FormEvent ev) {
+		String name = ev.getName();
+		String occupation = ev.getOccupation();
+		int ageCatId = ev.getAgeCategory();
+		String empCat = ev.getEmpCat();
+		boolean isUS = ev.isUsCitizen();
+		String taxId = ev.getTaxId();
+		String gender = ev.getGender();
+		
+		AgeCategory ageCategory = null;
+		
+		
+		// w zaleznosci od wyboru opcji przez uzytkownika programu (formularz) ageCatId przyjmuje wartosci 0 1 lub 2
+		switch(ageCatId) {
+		case 0: 
+			ageCategory = AgeCategory.child;
+			break;
+			
+		case 1: 
+			ageCategory = AgeCategory.adult;
+			break;
+			
+		case 2: 
+			ageCategory = AgeCategory.senior;
+			break;
+			
+		}
+		
+		
+		EmploymentCategory empCategory;
+		
+		if(empCat.equals("employed")) {
+			empCategory = EmploymentCategory.emplyed;
+			
+		}
+		else if(empCat.equals("self-employed")) {
+			empCategory = EmploymentCategory.selfEmplyed;
+			
+		}
+		else if(empCat.equals("unemployed")) {
+			empCategory = EmploymentCategory.unemployed;
+			
+		}
+		else {
+			empCategory = EmploymentCategory.other;
+			System.err.println(empCat);
+			
+		}
+		
+		Gender genderCat;
+		
+		if(gender.equals("male")) {
+			genderCat = Gender.male;
+		}
+		else {
+			genderCat = Gender.female;
+		}
+		
+		Person person = new Person(name, occupation, ageCategory, 
+				empCategory, taxId, isUS, genderCat);
+		db.addPerson(person);
+	}
+	
+}
---
+++ Java Swing tutorial/Swing1/src/gui/Toolbar.java
@@ -0,0 +1,81 @@
+package gui;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Toolbar extends JPanel implements ActionListener{
+	
+	private JButton helloButton;
+	private JButton goodbyeButton;
+	
+	private StringListener textListener;
+	
+	public Toolbar() {
+		
+		setBorder(BorderFactory.createEtchedBorder());
+		
+		helloButton = new JButton("Hello");
+		goodbyeButton = new JButton("Goodbye");
+		
+		helloButton.addActionListener(this);
+		goodbyeButton.addActionListener(this);
+		
+		
+		
+		// UWAGA - zastosowano tu Flow Layout
+		
+		//setLayout(new FlowLayout()); // przyciski beda ustawione na srodku panelu
+		
+		setLayout(new FlowLayout(FlowLayout.LEFT)); // przyciski po lewej stronie panelu
+		
+		add(helloButton);
+		add(goodbyeButton);
+	}
+	
+	
+
+
+
+
+	public void setStringListener(StringListener listener) {
+		this.textListener = listener;
+	}
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("A button has been clicked");
+		
+		JButton clicked = (JButton)e.getSource(); // getSource zwraca Object - ale w tym przypadku sa jedynie obiekty typu JButton - zatem stosuje sie rzutowanie
+	
+		if(clicked == helloButton) {
+			//System.out.println("Hello");
+			
+			//textPanel.appendText("Hello\n");
+			
+			if(textListener != null) {
+				textListener.textEmitted("Hello");
+			}
+			
+			
+		} else if (clicked == goodbyeButton) {
+			//System.out.println("Goodbye");
+			
+			//textPanel.appendText("Goodbye\n");
+			
+			if(textListener != null) {
+				textListener.textEmitted("Goodbye");
+			}
+			
+		}
+	
+	}
+	
+}
