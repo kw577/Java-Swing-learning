b379c9e1597d4b7d4513c18c380571167a6fc4d2
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2019-02-22 11:31:24 +0100

    Wielowatkowosc, update aplikacji z osobnego watku, SwingWorker (generalnie zaleca sie jednak ograniczac ilosc watkow w GUI)

--- Java Swing tutorial/Swing1/src/gui/MessagePanel.java
+++ Java Swing tutorial/Swing1/src/gui/MessagePanel.java
@@ -1,12 +1,16 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingWorker;
 import javax.swing.event.CellEditorListener;
 import javax.swing.event.ChangeEvent;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -107,11 +111,7 @@ public class MessagePanel extends JPanel {
 				}
 				messageServer.setSelectedServers(selectedServers);
 				
-				System.out.println("Messages waiting: " + messageServer.getMessageCount());
-				
-				for(Message message: messageServer) {
-					System.out.println(message.getTitle());
-				}
+				retrieveMessages();
 				
 			}
 			
@@ -120,6 +120,60 @@ public class MessagePanel extends JPanel {
 		setLayout(new BorderLayout());
 		
 		add(new JScrollPane(serverTree), BorderLayout.CENTER);
+	}
+	
+	private void retrieveMessages() {
+			
+		System.out.println("Messages waiting: " + messageServer.getMessageCount());
+		
+		SwingWorker<List<Message>, Integer> worker = new SwingWorker<List<Message>, Integer>(){
+
+			
+			@Override
+			protected void done() {
+				try {
+					List<Message> retrievedMessages = get();
+					System.out.println("Retrieved " + retrievedMessages.size() + " messages.");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			@Override
+			protected void process(List<Integer> counts) {
+				
+				int retrieved = counts.get(counts.size() - 1);
+				System.out.println("Got " + retrieved + " messages.");
+			}
+
+			@Override
+			protected List<Message> doInBackground() throws Exception {
+				
+				List<Message> retrievedMessages = new ArrayList<Message>();
+				int count = 0;
+				
+				for(Message message: messageServer) {
+					System.out.println(message.getTitle());
+					retrievedMessages.add(message);
+					
+					count++;
+					publish(count);
+					
+				}
+				
+				return retrievedMessages;
+			}
+			
+			
+			
+		};
+		
+		worker.execute();
+		
 	}
 	
 	
